108d1bbf14451c04e8abde6dc6da5059d7320b8e
jayant agarwal <01agarwal.jayant@gmail.com>
2024-08-28 09:38:02 +0530

    bash cripting

---
+++ hello_world.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This is a comment - anything after the "#" symbol is ignored by the shell
+
+# Print "Hello, World!" to the terminal
+echo "Hello, World!"
+
+# Exit the script with a success status code (0)
+exit 0
---
+++ script2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create a new directory
+mkdir my_new_dir
+
+# Navigate into the new directory
+cd my_new_dir
+
+# Print the current working directory
+echo "You are now in: $(pwd)"
+exit 0
+
